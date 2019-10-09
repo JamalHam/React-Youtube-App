66a0be22d5edbdd58681dea90d36be345977ce6c
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-10-08 21:16:38 -0400

    basic serach bar and linked stylesheet

---
+++ src/apis/youtube.js
@@ -0,0 +1,2 @@
+const KEY = 'AIzaSyBe9iS1F_CRfCqv6qos2PvDLcU7kU1MRXU';
+
---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = {term: 'whats up homie'};
+
+    onInputChange = (event) =>{
+        this.setState({ term: event.target.value });
+    };
+
+    onFormSubmit = (event) =>{
+        event.preventDefault();
+        // prevent auto refresh
+    };
+
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input 
+                            type="text" 
+                            value={this.state.term}
+                            onChange={this.onInputChange} 
+                            />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
