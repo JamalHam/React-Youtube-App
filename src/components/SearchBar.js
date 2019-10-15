aae7a0e6403d11ad3ac2ed1e0864443fdd399819
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-10-14 21:21:08 -0400

    finished app, search and display top 5 results with thumbnail and main image view

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = {term: 'whats up homie'};
+    state = {term: ''};
 
     onInputChange = (event) =>{
         this.setState({ term: event.target.value });
